45bd2876b406e03c603694c5339ca5c4dc26db05
Navodya <nawodhyasrilanka2015@gmail.com>
2026-01-04 14:22:29 +0530

    added search bar section and search input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {PropertyCard} from './propertyCard';
 import propertiesData from './properties.json';
 
 function App() {
+  // filter and search states
+  const [searchTerm, setSearchTerm] = useState("")
   console.log("PropertyCard is:", PropertyCard);
   return (
     <div className="App">
@@ -13,6 +15,19 @@ function App() {
 
       <main style={{ padding: '20px' }}>
         <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Latest Properties</h2>
+                {/* Search Section */}
+        <div style={{ display: 'flex', justifyContent: 'center', gap: '15px', marginBottom: '30px', flexWrap: 'wrap' }}>
+          
+          {/* Search Input */}
+          <input 
+            type="text" 
+            placeholder="Search what you need" 
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            style={{ padding: '10px', width: '300px', fontSize: '16px' }}
+          />
+          </div>
+
         
         {/* Property List Container */}
         <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
